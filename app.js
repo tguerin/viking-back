2750da81dbe8fc0836f163e21cd77c4ba2686cc1
Thomas Guerin <thomas.guerin2@gmail.com>
2014-09-03 14:26:20 +0200

    Add server logic + fake data

--- app.js
+++ app.js
@@ -8,7 +8,18 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+require('./module/realTimeAggregation');
+
 var app = express();
+
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +29,7 @@ app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+app.use(allowCrossDomain);
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
---
+++ module/realTimeAggregation.js
@@ -0,0 +1,49 @@
+var app = require('../app');
+var io = require('socket.io')(app);
+var mqtt = require('mqtt');
+var uuid = require('node-uuid');
+
+var client = mqtt.createClient(1883, 'ec2-54-186-153-191.us-west-2.compute.amazonaws.com');
+
+var dataMap = {};
+
+client.publish('source_sub', '{"id":"29f36f37-4ede-4edb-af0e-f7b74c144d5a","keywords":["android","ios","phonegap"]}');
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('front-request', function (message) {
+    var serverId = uuid.v4();
+    dataMap[serverId] = {socket: socket, aggregation: {id: serverId, positive: 0, neutral: 0, negative: 0}};
+    var messageToCollector = JSON.stringify({id: serverId, keywords: message.keywords});
+    console.log(messageToCollector);
+    client.publish('source_sub', messageToCollector);
+    socket.emit('front-request', {clientId: message.clientId, serverId: serverId});
+    setInterval(function() {
+      dataMap[serverId].aggregation.positive++;
+      dataMap[serverId].aggregation.neutral+=2;
+      dataMap[serverId].aggregation.negative+=3;
+      socket.emit('front-response', dataMap[serverId].aggregation);
+    }, 3000);
+
+  });
+});
+
+client.subscribe('processed_data');
+
+client.on('message', function (topic, message) {
+  console.log(topic + ' ' + message);
+  var tweetWithFeeling = JSON.parse(message);
+  if (tweetWithFeeling.id in dataMap) {
+    var data = dataMap[tweetWithFeeling.id];
+    if (tweetWithFeeling.sentiment < 2) {
+      data.aggregation.negative++;
+    } else if (tweetWithFeeling.sentiment > 2) {
+      data.aggregation.positive++;
+    } else {
+      data.aggregation.neutral++;
+    }
+    data.socket.emit('front-response', data.aggregation);
+  }
+});
+
+io.listen(8888);
